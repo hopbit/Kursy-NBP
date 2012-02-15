a67f111bfcc1b4c7e5be340bcadcb1ba9a1413e1
Łukasz Siwiński <lsiwinski@gmail.com>
2012-02-15 23:30:20 +0100

    Initial commit on project Kursy NBP

---
+++ src/info/siwinski/kusynpb/services/NBPService.java
@@ -0,0 +1,12 @@
+package info.siwinski.kusynpb.services;
+
+import java.sql.Date;
+
+public class NBPService {
+
+	public Date getNbpRatesDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/info/siwinski/kusynpb/services/UtilsService.java
@@ -0,0 +1,5 @@
+package info.siwinski.kusynpb.services;
+
+public class UtilsService {
+
+}
